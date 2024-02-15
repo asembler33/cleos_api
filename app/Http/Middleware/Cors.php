f15d7383f4c3090246820d71450cd3be67021288
asembler33 <ocarrasco20@gmail.com>
2024-02-15 20:59:37 -0300

    Cambios en CORS

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -20,6 +20,7 @@ class Cors
         $response->header('Access-Control-Allow-Origin', '*');
         $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         $response->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        $response->header('Access-Control-Allow-Credentials', 'true');
 
         return $response;
     }
